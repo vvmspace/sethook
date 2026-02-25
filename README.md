53a7e43bbc304d52175223bce76361313b8a08d2
Vladimir V. M. <vvmspace@gmail.com>
2026-02-25 21:16:09 +0400

    feat: add support for -d/--drop flag

--- README.md
+++ README.md
@@ -26,6 +26,7 @@ npx sethook 123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11
 
 - `token` - The Telegram Bot API token
 - `url` - The URL to set the webhook to
+- `-d`, `--drop` - (Optional) Drop pending updates by adding `drop_pending_updates=true` to the request
 
 
 ## Description
--- sethook.sh
+++ sethook.sh
@@ -1,11 +1,34 @@
-# usage: sethook telegram_bot_token <url_to_send_updates_to>
+#!/usr/bin/env bash
+
+# usage: sethook telegram_bot_token <url_to_send_updates_to> [-d|--drop]
 # https://api.telegram.org/bot{my_bot_token}/setWebhook?url={url_to_send_updates_to}
 # echo description from response json
 
-if [ -z "$1" ]; then
+DROP_PENDING=""
+TOKEN=""
+URL=""
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        -d|--drop)
+            DROP_PENDING="&drop_pending_updates=true"
+            shift
+            ;;
+        *)
+            if [ -z "$TOKEN" ]; then
+                TOKEN="$1"
+            elif [ -z "$URL" ]; then
+                URL="$1"
+            fi
+            shift
+            ;;
+    esac
+done
+
+if [ -z "$TOKEN" ]; then
     echo "Usage:"
-    echo "npx sethook telegram_bot_token <url_to_send_updates_to>"
+    echo "npx sethook <telegram_bot_token> [url_to_send_updates_to] [-d|--drop]"
     exit 1
 fi
 
-curl -s https://api.telegram.org/bot$1/setWebhook?url=$2 | jq -r '.description'
+curl -s "https://api.telegram.org/bot${TOKEN}/setWebhook?url=${URL}${DROP_PENDING}" | jq -r '.description'
